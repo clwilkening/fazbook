846d63edd27427318144e5bad2ff4b5274b50bcf
Chris Wilkening <clw13clw13@gmail.com>
2017-01-26 18:17:10 -0500

    add delete functionality

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../server/models/index');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('users/index', { title: 'fazbook' });
+  models.User.findAll({}).then(function(users) {
+  res.render('users/index', {
+    title: 'fazbook',
+    users: users
+    });
+  });
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('users/new', {title: 'new facebook users'});
+});
+
+router.post('/', function(req, res, next) {
+  models.User.create({
+    email: req.body.email,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    dob: req.body.dob
+  }).then(function(users) {
+    res.redirect('/users');
+  });
+});
+
+router.delete('/:id', function(req, res, next) {
+  models.User.destroy({
+    where: { id: req.params.id }
+  }).then(function(user) {
+    res.redirect('/users');
+  });
 });
 
 module.exports = router;
