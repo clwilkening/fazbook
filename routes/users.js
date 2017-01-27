cc1b698816a247ee5d06788777c8af8508124ce2
Chris Wilkening <clw13clw13@gmail.com>
2017-01-26 19:42:53 -0500

    add ability to update user

--- routes/users.js
+++ routes/users.js
@@ -35,4 +35,29 @@ router.delete('/:id', function(req, res, next) {
   });
 });
 
+router.get('/:id/edit', function(req, res, next) {
+  models.User.findById(req.params.id).then(function(user) {
+    res.render('users/edit', { user: user });
+  });
+});
+
+router.get('/:id/show', function(req, res, next) {
+  models.User.findById(req.params.id).then(function(user) {
+  res.render('users/show', {
+    user: user
+    });
+  });
+});
+
+router.put('/:id', function(req, res, next) {
+  models.User.update({
+    email: req.body.email,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    dob: req.body.dob
+  }, { where: { id: req.params.id } }).then(function() {
+    res.redirect('/users/');
+  });
+});
+
 module.exports = router;
